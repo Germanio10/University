152ed00767eccfc40109628e80345de277386667
German Chaley <chaleyg2@gmail.com>
2023-11-02 22:36:11 +0300

    add handlers create_user and routers

---
+++ api/handlers.py
@@ -0,0 +1,33 @@
+from fastapi.routing import APIRouter
+from models import UserCreate, ShowUser
+from db.session import async_session
+from db.dals import UserDAL
+
+user_router = APIRouter()
+
+#####################
+# BLOCK CREATE USER #
+#####################
+
+
+async def _create_user(body: UserCreate):
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active
+            )
+
+
+@user_router.post("/", response_model=ShowUser)
+async def create_user(body: UserCreate):
+    return await _create_user(body)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import uvicorn
 from fastapi.routing import APIRouter
-from sqlalchemy import Column, Boolean, String
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
-import settings
-from sqlalchemy.dialects.postgresql import UUID
-import uuid
-from pydantic import BaseModel, EmailStr, validator
+from api.handlers import user_router
+
+app = FastAPI(title="University")
+
+main_api_router = APIRouter()
+
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
